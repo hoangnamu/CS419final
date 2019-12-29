5f7cdfbadd4ee045f8e8d83b5e2555fe2179c8a8
hoangnam <hoangnamle1998@gmail.com>
2019-12-29 23:24:32 +0700

    sua theo book

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import {
+  ReactiveBase,
+  DataSearch,
+  SingleRange,
+  ResultCard
+} from "@appbaseio/reactivesearch";
+import src from "*.bmp";
+class App extends Component {
+  render() {
+    return (
+      <ReactiveBase
+        app="CS419final"
+        credentials="6q4HVxGDL:b7c3e659-e37f-4292-86fd-29233fc525be"
+      >
+        <DataSearch
+          componentId="mainSearch"
+          dataField={["original_title", "authors"]}
+          queryFormat="and"
+          iconPosition="left"
+        />
+        <SingleRange
+          componentId="ratingsFilter"
+          dataField="average_rating_rounded"
+          title="Book Ratings"
+          data={[
+            { start: 4, end: 5, label: "★★★★ & up" },
+            { start: 3, end: 5, label: "★★★ & up" },
+            { start: 2, end: 5, label: "★★ & up" },
+            { start: 1, end: 5, label: "★ & up" }
+          ]}
+          react={{
+            and: "mainSearch"
+          }}
+        />
+        <ResultCard
+          componentId="results"
+          dataField="original_title"
+          react={{
+            and: ["mainSearch"]
+          }}
+          onData={res => ({
+            image: res.image_url,
+            title: res.original_title,
+            description: res.average_rating + " ★ "
+          })}
+        />
+      </ReactiveBase>
+    );
+  }
 }
-
 export default App;
